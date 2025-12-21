88553019c975cc6ffe95032a479696b24ee9ff7b
Maaz Mallak <maaz.mallak@gmail.com>
2025-12-21 21:13:27 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "cw-game-a42eb",
   storageBucket: "cw-game-a42eb.firebasestorage.app",
   messagingSenderId: "767941381110",
-  appId: "1:767941381110:web:46e8d3d816ac6c6d6c95d7"
+  appId: "1:767941381110:web:d06274637ddd92226c95d7"
 };
 
 const app = initializeApp(firebaseConfig);
